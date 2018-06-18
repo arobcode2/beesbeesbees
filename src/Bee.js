6f23eefcb21fa193f16cd321ce211390cfd5c6f5
arobcode2 <andrew-c-rob@live.com>
2018-06-18 11:47:15 -0500

    complete Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+  
 };
 
+Bee.prototype = Object.create(Grub.prototype)
+// Bee.prototype.eat = function() {
+Bee.prototype.constructor = Bee
+// };
+
+var bee = new Bee();
